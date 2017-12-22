4ade7a160d335a2300bf0141c8e80aeb92f82e12
t_rappos <t_rappos@hotmail.com>
2017-12-23 10:20:15 +1100

    cleaned up repo

--- examples/create-react-app/my-app/db/db_config.js
+++
@@ -1,9 +0,0 @@
-
-//THIS FILE IS LOCAL ONLY AND NOT TO BE UPLOADED TO GIT
-//ANY CODE LOADING THIS FILE MUST PREPARE FOR IT TO NOT BE AVAILABLE AND
-//USE DATABASE_URL env variables instead
-
-module.exports = {
-    config: 'postgresql://postgres:yulittle@localhost:5432/postgres'
-  };
-  
